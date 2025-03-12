752f377ad01b40da9695a899bab880a184bb6a8e
seal <sergej.aleman@infpro.com>
2025-03-12 19:38:48 +0100

    Clean mvvm

--- ViewModels/WindowViewModels/NavigationViewModel.cs
+++ ViewModels/WindowViewModels/NavigationViewModel.cs
@@ -8,7 +8,6 @@ namespace TFS_UI_mvvm.ViewModels.WindowViewModels;
 public class NavigationViewModel : BaseViewModel
 {
     public string Title { get; set; } = "MyTitle";
-    //private readonly IServiceProvider serviceProvider;
 
     private object? currentView;
 
@@ -24,63 +23,20 @@ public class NavigationViewModel : BaseViewModel
 
 
     #region Commands
+    public RelayCommand? SwitchToMainCommand { get; }
     public RelayCommand? SwitchToACommand { get; }
     public RelayCommand? SwitchToBCommand { get; }
     #endregion
 
 
-    #region Constructors
-    ////Empty constructor for design time
-    //public MainWindowViewModel()
-    //{
-    //    if (!IsInDesignMode())
-    //        throw new InvalidOperationException("ServiceProvider is required at runtime");
-    //}
-
-
-
+    #region Constructor
     public NavigationViewModel()
     {
-        //this.serviceProvider = serviceProvider;
-
         CurrentView = new MainUserControlViewModel();
-
+        
+        SwitchToMainCommand = new RelayCommand(_ => CurrentView = new MainUserControlViewModel());
         SwitchToACommand = new RelayCommand(_ => CurrentView = new UserControlA_ViewModel());
         SwitchToBCommand = new RelayCommand(_ => CurrentView = new UserControlB_ViewModel());
-
-
-
-
-        //SwitchToACommand = new RelayCommand(_ => ShowUserControlA());
-        //SwitchToBCommand = new RelayCommand(_ => ShowUserControlB());
-
-        //ShowUserControlA();
     }
-    #endregion
-
-
-    #region Methods
-    //private void ShowUserControlA()
-    //{
-    //    //if (serviceProvider == null) throw new InvalidOperationException("ServiceProvider is required at runtime");
-    //    var view = serviceProvider.GetRequiredService<UserControlA>();
-    //    view.DataContext = serviceProvider.GetRequiredService<UserControlA_ViewModel>();
-    //    CurrentView = view;
-    //}
-
-
-    //private void ShowUserControlB()
-    //{
-    //    //if (serviceProvider == null) throw new InvalidOperationException("ServiceProvider is required at runtime");
-    //    var view = serviceProvider.GetRequiredService<UserControlB>();
-    //    view.DataContext = serviceProvider.GetRequiredService<UserControlB_ViewModel>();
-    //    CurrentView = view;
-    //}
-
-    ////For design time
-    //private bool IsInDesignMode()
-    //{
-    //    return System.ComponentModel.DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject());
-    //} 
     #endregion
 }
